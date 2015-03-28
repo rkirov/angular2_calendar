ccd48b79aa5fb298b213f1a17cb13c2d4dd1ea0a
Igor Minar <igor@angularjs.org>
2015-03-27 23:14:29 -0700

    WIP: install all dependencies in serve.sh

--- serve.sh
+++ serve.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-nodemon --watch Brocfile.js --watch cauliflower/index.js --watch broccoli-typescript/index.js cauliflower
+npm install
+npm install nodemon
+cd cauliflower
+npm install
+cd ../broccoli-typescript
+npm install
+cd ..
+$(npm bin)/nodemon --watch Brocfile.js --watch cauliflower/index.js --watch broccoli-typescript/index.js cauliflower
